5188f75ca5f47c4e7bd625227f2636c9996cb3be
jhonatanAlanFerreira <jhonatan.alanf@gmail.com>
2021-10-03 22:56:09 -0300

    Pegando a proxima operacao

--- classes/Calculator.js
+++ classes/Calculator.js
@@ -9,12 +9,52 @@ module.exports = class Calculator {
     }
 
     execute() {
-        let operation = this._getNextOperation();
+        let operationPosition = this._getNextOperationPosition();
+        let {
+            start,
+            end
+        } = operationPosition;
+        end++;
+
+        let operation = this._expression.slice(start, end);
+        let result = this._getResult(operation); //falta implementar, será o resultado da operação
+
+        let exp = this._expression;
+        this._expression = exp.substring(0, start) + result + exp.substring(end);
+
+        //linhas de teste, serão removidas
         console.log(operation);
+        console.log(this._expression);
     }
 
-    _getNextOperation() {
-        return this._expression;
+    _getNextOperationPosition() {
+        let openIn = -1;
+        let closeIn = -1;
+
+        for (let i = 0; i < this._expression.length; i++) {
+            let l = this._expression[i];
+
+            if (l == '(') openIn = i;
+            if (l == ')') {
+                closeIn = i;
+                break;
+            }
+        }
+
+        if (openIn > -1 && closeIn > -1) return {
+            start: openIn,
+            end: closeIn
+        };
+
+        return {
+            start: 0,
+            end: this._expression.length
+        };
+    }
+
+    //falta implementar
+    _getResult(operation) {
+        return 0;
     }
 
 }
